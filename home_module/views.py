be26e3258303695d82ed30fb73eeda1775f37c8a
Bahman Pournazari <bahmanpn@gmail.com>
2024-12-10 15:36:57 +0330

    seperated navbar+footer+copyright+headers,just need to know which items must remove

--- home_module/views.py
+++ home_module/views.py
@@ -10,5 +10,14 @@ class homePageView(View):
 def header_component(request):
     return render(request,'header_component.html')
 
+def mobile_sidebar_component(request):
+    return render(request,'mobile_sidebar_component.html')
+
+def navbar_component(request):
+    return render(request,'navbar_component.html')
+
 def footer_component(request):
     return render(request,'footer_component.html')
+
+def copyright_component(request):
+    return render(request,'copyright_component.html')
